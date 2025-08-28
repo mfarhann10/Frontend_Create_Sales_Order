d53048b071a70f5157cba396397c8bf364f898fd
mfarhann10 <mfarhann1009@gmail.com>
2025-08-29 05:42:52 +0700

    test production

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SalesOrderForm from "./pages/SalesOrderForm";
-import { Test } from "./pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SalesOrderForm />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
