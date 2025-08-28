34d9f000697d498454dd6778bc88acbcd106e829
mfarhann10 <mfarhann1009@gmail.com>
2025-08-28 14:36:33 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <div className='flex items-center justify-between align-middle'>
+      <p className='text-blue-400 text-center font-bold'>Test</p>
+    </div>
+  )
+}
+
+export default App
